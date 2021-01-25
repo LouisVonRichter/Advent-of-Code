9de173b4ea94e8201c74d1349cba30df29c71564
LouisVonR <louisvonrichter@gmail.com>
2021-01-26 08:31:25 +1000

    Day 1 Answers

---
+++ 2020/Day-1/Day-1.py
@@ -0,0 +1,15 @@
+with open('day-1.txt', 'r') as data:
+    num = data.readlines()
+
+num = [int(i.split("\n")[0]) for i in num]
+
+print("Question 1\n")
+for i in num:
+    if 2020-i in num:
+        print(i, 2020-i, i*(2020-i))
+
+print("\nQuestion 2\n")
+for i in num:
+    for j in num:
+        if 2020-i - j in num:
+            print(2020-i - j, i, j, 2020-i * i * j)
