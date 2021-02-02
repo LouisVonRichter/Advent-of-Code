6ac2a74d6c863e2be7e68ddc2369c2a178200114
LouisVonR <louisvonrichter@gmail.com>
2021-02-02 14:52:58 +1000

    Day 5 Answers

---
+++ 2020/Day-5/Day-5.py
@@ -0,0 +1,35 @@
+with open('day-5.txt', 'r') as data:
+    seats = data.readlines()
+
+seats = [line[:-1] for line in seats]
+
+seat_ids = []
+for seat in seats:
+    r = seat[:7]
+    start = 0
+    end = 127
+    row, col = 0, 0
+    for letter in r:
+        if letter == "F":
+            end = int((start + end + 1) / 2) - 1
+        elif letter == "B":
+            start = int((start + end + 1) / 2)
+    row = start
+
+    r = seat[7:]
+    start = 0
+    end = 7
+    for letter in r:
+        if letter == "L":
+            end = int((start + end + 1) / 2) - 1
+        elif letter == "R":
+            start = int((start + end + 1) / 2)
+    col = start
+
+    sid = (row * 8) + col
+    seat_ids.append(sid)
+
+
+print(f"Solution 1: {max(seat_ids)}")
+print(f"Solution 2: {[seat for seat in range(min(seat_ids), max(seat_ids)) if seat not in seat_ids][0]}")
+
