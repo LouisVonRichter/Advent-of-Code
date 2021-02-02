f8275125c09de7e29152496b121a731d021dd70a
LouisVonR <louisvonrichter@gmail.com>
2021-02-02 15:38:27 +1000

    Day 6 Answers

---
+++ 2020/Day-6/Day-6.py
@@ -0,0 +1,31 @@
+with open("day-6.txt", "r") as data:
+    lines=data.readlines()
+
+groups = []
+group = []
+for question in lines:
+    if question!="\n":
+        group.append(question.split("\n")[0])
+    else:
+        groups.append(group)
+        group=[]
+groups.append(group)
+
+solution_1 = []
+for group in groups:
+    unique_ques = []
+    for ques in group:
+        unique_ques.extend([uq for uq in ques])
+
+    solution_1.extend(list(set(unique_ques)))
+print(f"Solution 1: {len(solution_1)}")
+
+from collections import Counter
+total = 0
+for group in groups:
+    group_size = len(group)
+    counts = Counter("".join(group))
+
+    counts = Counter(list(counts.values()))[group_size]
+    total+=counts
+print(f"Solution 2:", total)
